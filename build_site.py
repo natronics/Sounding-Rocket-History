91a4013e7220825c33a06991f65fbae523426c27
Nathan Bergey <nathan.bergey@gmail.com>
2013-09-09 14:29:46 -0700

    pure proof of concept for dynamically bulding data

---
+++ build_site.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from dateutil import parser
+
+all_flights = []
+
+
+with open('rawdata/sounding-rocket-history.csv', 'r') as f_in:
+    for line in f_in:
+        if line[0] != '#':
+            li = line.split(',')
+            date     = parser.parse((li[0]).strip())
+            vehicle  =               li[1].strip()
+            location =               li[2].strip()
+
+            all_flights.append({'date': date, 'vehicle': vehicle, 'location': location})
+
+
+for flight in all_flights:
+    with open('_posts/%d-12-31-00-00-00-Data.textile' % flight['date'].year, 'w') as post:
+        post.write("""---
+layout: base
+title: data
+---
+
+h1. Data for %d
+
+ * %s, %s, %s
+"""  % (flight['date'].year, flight['date'].isoformat(), flight['vehicle'], flight['location']))
--- index.markdown
+++ index.markdown
@@ -6,3 +6,9 @@ title: Sounding Rocket History
 # Placeholder
 
 For now, this doesn't exist. Sorry.
+
+<ul>
+{% for post in site.posts %}
+    <li><a href="{{ post.url }}">{{ post.date | date: "%Y" }}</a></li>
+{% endfor %}
+</ul>
