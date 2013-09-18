92ecf160f1d9a88e0b44da1e71e984b215c3fe1e
Nathan Bergey <nathan.bergey@gmail.com>
2013-09-18 14:20:12 -0700

    launch site table

--- add_record.py
+++ add_record.py
@@ -8,7 +8,12 @@ lv = models.Vehicle(name='Nike Asp')
 db.session.add(lv)
 db.session.commit()
 
+site = models.Site(name='Wallops Flight Facility', lat=37.8445, lon=-75.4798, country="USA")
+db.session.add(site)
+db.session.commit()
 
-lch = models.Launch(time=datetime.datetime(1960, 3, 1, 22, 11), designation='3.01 GS', success=models.SUCCESS, vehicle=lv)
+
+
+lch = models.Launch(time=datetime.datetime(1960, 3, 1, 22, 11), designation='3.01 GS', success=models.SUCCESS, vehicle=lv, site=site)
 db.session.add(lch)
 db.session.commit()
--- database/models.py
+++ database/models.py
@@ -5,11 +5,20 @@ UNKNOWN, SUCCESS, PARTIAL_SUCCESS, FAILURE = range(4)
 
 class Vehicle(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128))
+    name = db.Column(db.String(128), nullable=False)
     launches = db.relationship('Launch', backref='vehicle', lazy='dynamic')
 
     def __repr__(self):
         return '<vehicle %s (%d)>' % (self.name, self.id)
+
+
+class Site(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    lat = db.Column(db.Float, nullable=False)
+    lon = db.Column(db.Float, nullable=False)
+    country = db.Column(db.String(128))
+    launches = db.relationship('Launch', backref='site', lazy='dynamic')
 
 
 class Launch(db.Model):
@@ -18,6 +27,8 @@ class Launch(db.Model):
     designation = db.Column(db.String(64))
     success = db.Column(db.SmallInteger, default=UNKNOWN)
     lv_id = db.Column(db.Integer, db.ForeignKey('vehicle.id'))
+    site_id = db.Column(db.Integer, db.ForeignKey('site.id'))
+
 
     def __repr__(self):
         return '<launch %d>' % self.id
