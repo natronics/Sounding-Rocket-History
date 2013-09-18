8e76013c0af05bef6a43a7dc750fb7adf372e940
Nathan Bergey <nathan.bergey@gmail.com>
2013-09-18 16:04:13 -0700

    updateable vehicle page

--- editor.py
+++ editor.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask.ext.sqlalchemy import SQLAlchemy
 import config
 
@@ -14,10 +14,17 @@ def index():
     launches = database.models.Launch.query.all()
     return render_template('index.html', launches=launches)
 
-@app.route("/vehicle/<int:lvid>")
+@app.route("/vehicle/<int:lvid>", methods=['GET', 'POST'])
 def vehicle(lvid):
     vehicle = database.models.Vehicle.query.get(lvid)
-    return render_template('vehicle.html', vehicle=vehicle)
+    updated = False
+    if request.method == 'POST':
+        vehicle.name = request.form['vehicle.name']
+        vehicle.desc = request.form['vehicle.desc']
+        db.session.commit()
+        updated = True
+
+    return render_template('vehicle.html', vehicle=vehicle, updated=updated)
 
 if __name__ == "__main__":
     app.debug = True
