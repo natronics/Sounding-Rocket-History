f50ad587083330d1aa648a36f4b5e7029c4ffade
Nathan Bergey <nathan.bergey@gmail.com>
2013-11-12 13:47:20 -0800

    A reasonable data model, I think

--- database/models.py
+++ database/models.py
@@ -1,69 +1,93 @@
 from editor import db
 
-UNKNOWN, SUCCESS, PARTIAL_SUCCESS, FAILURE = range(4)
-
 
 class Vehicle(db.Model):
-    """A launch vehicle (rocket)."""
+    """A launch vehicle (rocket).
+    """
 
     __tablename__ = 'vehicle'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128), nullable=False)
-    desc = db.Column(db.UnicodeText())
-    length = db.Column(db.Float())
-    width = db.Column(db.Float())
-    launches = db.relationship('Launch', backref='vehicle', lazy='dynamic')
+    id              = db.Column(db.Integer, primary_key=True)
+    name            = db.Column(db.String(128), nullable=False)
+    desc            = db.Column(db.UnicodeText())
+    length          = db.Column(db.Float())
+    width           = db.Column(db.Float())
+    launches        = db.relationship('Launch', backref='vehicle', lazy='dynamic')
 
     def __repr__(self):
         return '%s [%d]' % (self.name, self.id)
 
 
 class Site(db.Model):
-    """A launch site"""
+    """The facility, not individual pad where launches occur has a link to the country
+    it's in or is operated by, in the case of territories
+    """
 
     __tablename__ = 'site'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128), nullable=False)
-    desc = db.Column(db.UnicodeText())
-    lat = db.Column(db.Float, nullable=False)
-    lon = db.Column(db.Float, nullable=False)
-    country = db.Column(db.String(128))
-    launches = db.relationship('Launch', backref='site', lazy='dynamic')
+    name            = db.Column(db.String(128), nullable=False)
+    abbrv           = db.Column(db.String(16), nullable=False)
+    desc            = db.Column(db.UnicodeText())
+    latitude        = db.Column(db.Float, nullable=False)
+    longitude       = db.Column(db.Float, nullable=False)
+    launches        = db.relationship('Launch', backref='site', lazy='dynamic')
+    country_id      = db.Column(db.Integer, db.ForeignKey('country.id'))
+    country         = db.relationship('Country', backref='site')
 
     def __repr__(self):
-        return '%s [%d]' % (self.name, self.id)
+        return str(self.name)
 
 
 class LaunchSuccess(db.Model):
+    """Many launches have some information about if they worked. This is strictly
+    for the physical launch vehicle itself. Further information about payload success
+    sould be kept somewhere else
+    """
 
     __tablename__ = 'launchsuccess'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128), nullable=False)
-    desc = db.Column(db.UnicodeText())
+    id              = db.Column(db.Integer, primary_key=True)
+    name            = db.Column(db.String(128), nullable=False)
+    desc            = db.Column(db.UnicodeText())
+
+    def __repr__(self):
+        return str(self.name)
+
+
+class Country(db.Model):
+    """A country, mostly so the flag and geo data can be kept in one place.
+    """
+
+    __tablename__ = 'country'
+
+    id              = db.Column(db.Integer, primary_key=True)
+    name            = db.Column(db.String(128), nullable=False)
+    geo_outline     = db.Column(db.UnicodeText())
+    flag            = db.Column(db.UnicodeText(), nullable=False)
 
     def __repr__(self):
         return str(self.name)
 
 
 class Launch(db.Model):
-    """Info about single launch"""
+    """Information for single launch that occured. Links back to the vehicle
+    and launch site. There are medatadata fields as well. This is the primary
+    chunck of info for the site
+    """
 
     __tablename__ = 'launch'
 
-    id = db.Column(db.Integer, primary_key=True)
-    time = db.Column(db.DateTime, index=True, unique=False)
-    designation = db.Column(db.String(64))
-    lv_id = db.Column(db.Integer, db.ForeignKey('vehicle.id'))
-    site_id = db.Column(db.Integer, db.ForeignKey('site.id'))
-    lv = db.relationship('Vehicle', backref='launch')
-    ls = db.relationship('Site', backref='launch')
-    result_id = db.Column(db.Integer, db.ForeignKey('launchsuccess.id'))
-    result = db.relationship('LaunchSuccess', backref='launch')
-
-
+    id              = db.Column(db.Integer, primary_key=True)
+    time            = db.Column(db.DateTime, index=True, unique=False)
+    designation     = db.Column(db.String(64))
+    lv_id           = db.Column(db.Integer, db.ForeignKey('vehicle.id'), nullable=False)
+    launch_vehicle  = db.relationship('Vehicle', backref='launch')
+    site_id         = db.Column(db.Integer, db.ForeignKey('site.id'), nullable=False)
+    launch_site     = db.relationship('Site', backref='launch')
+    result_id       = db.Column(db.Integer, db.ForeignKey('launchsuccess.id'), nullable=False)
+    result          = db.relationship('LaunchSuccess', backref='launch')
+    apogee          = db.Column(db.Float, nullable=True)
 
     def __repr__(self):
-        return '<launch %d>' % self.id
+        return 'launch %s [%d]' % (self.designation, self.id)
--- editor.py
+++ editor.py
@@ -17,27 +17,38 @@ def index():
 
 class SiteAdmin(sqla.ModelView):
     column_display_pk = False
-    form_columns = ['name', 'desc', 'lat', 'lon', 'country']
+    column_list = ('name', 'latitude', 'longitude', 'country')
+    form_columns = ['name', 'abbrv', 'desc', 'latitude', 'longitude', 'country']
 
 class VehicleAdmin(sqla.ModelView):
     column_display_pk = False
+    column_list = ('name', 'length', 'width')
     form_columns = ['name', 'desc', 'length', 'width']
 
 class LaunchAdmin(sqla.ModelView):
-    column_display_pk = True
-    form_columns = ['time', 'designation', 'result', 'lv', 'ls']
+    column_display_pk = False
+    column_list = ('time', 'designation', 'launch_vehicle', 'launch_site', 'result',  'apogee')
+    form_columns = ['time', 'designation', 'launch_vehicle', 'launch_site', 'result',  'apogee']
 
 class ResultAdmin(sqla.ModelView):
-    column_display_pk = True
+    column_display_pk = False
     form_columns = ['name', 'desc']
 
+class CountryAdmin(sqla.ModelView):
+    column_display_pk = False
+    column_list = ('name',)
+    form_columns = ['name', 'geo_outline', 'flag']
+
+
 if __name__ == "__main__":
-    # Create admin
-    admin = admin.Admin(app, 'Sound Rocket Models')
+    # Create admin interface
+    admin = admin.Admin(app, 'Sound Rocket Database')
     admin.add_view(SiteAdmin(Site, db.session))
     admin.add_view(VehicleAdmin(Vehicle, db.session))
     admin.add_view(LaunchAdmin(Launch, db.session))
     admin.add_view(ResultAdmin(LaunchSuccess, db.session))
+    admin.add_view(CountryAdmin(Country, db.session))
 
+    # Run local webserver
     app.debug = True
     app.run()
